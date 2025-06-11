c5dffb25e299cff1941d4ef10a280def7886b4a1
Pratham Garg <prathamgarg0445@gmail.com>
2025-06-11 15:32:26 +0530

    Feat: added unimplemented error for problems api

--- src/controllers/problemController.js
+++ src/controllers/problemController.js
@@ -1,23 +1,40 @@
+import { StatusCodes } from "http-status-codes";
+
 export const pingProblem = (req,res) => {
     return res.json({message : "Problem ping is called"});
 }
 
 export const addProblem = async (req,res) => {
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED)
+    .json({
+        message : " Function not implemeneted yet "
+    });
 }
 
 export const getProblem = async (req,res) => {
-    
+    return res.status(StatusCodes.NOT_IMPLEMENTED)
+    .json({
+        message : " Function not implemeneted yet "
+    });
 }
 
 export const getProblems = async (req,res) => {
-    
+    return res.status(StatusCodes.NOT_IMPLEMENTED)
+    .json({
+        message : " Function not implemeneted yet "
+    });
 }
 
 export const deleteProblem = async (req,res) => {
-    
+    return res.status(StatusCodes.NOT_IMPLEMENTED)
+    .json({
+        message : " Function not implemeneted yet "
+    });
 }
 
 export const updateProblem = async (req,res) => {
-    
+    return res.status(StatusCodes.NOT_IMPLEMENTED)
+    .json({
+        message : " Function not implemeneted yet "
+    });
 }
